c5704f573587b23c4955844c304b5ffc890fc211
Sebastian <sebastian@squidex.io>
2021-01-08 22:37:46 +0100

    Missing field committed.

--- backend/src/Notifo.Infrastructure/MongoDb/Field.cs
+++ backend/src/Notifo.Infrastructure/MongoDb/Field.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ==========================================================================
+//  Notifo.io
+// ==========================================================================
+//  Copyright (c) Sebastian Stehle
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System;
+using MongoDB.Bson.Serialization;
 
 namespace Notifo.Infrastructure.MongoDb
 {
-    class Field
+    public static class Field
     {
+        public static string Of<T>(Func<T, string> mapper)
+        {
+            var name = mapper(default!);
+
+            return BsonClassMap.LookupClassMap(typeof(T)).GetMemberMap(name).ElementName;
+        }
     }
 }
