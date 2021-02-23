4b2f1f3f859921634d1d1ec92265b93f99c917fb
Sebastian <sebastian@squidex.io>
2021-02-23 15:16:30 +0100

    Added missing file.

---
+++ frontend/src/app/pages/events/EventDetails.tsx
@@ -0,0 +1,41 @@
+/*
+ * Notifo.io
+ *
+ * @license
+ * Copyright (c) Sebastian Stehle. All rights reserved.
+ */
+
+import { EventDto } from '@app/service';
+import * as React from 'react';
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-github';
+
+const style: React.CSSProperties = {
+    fontSize: '14px',
+    fontWeight: 'normal',
+    width: '100%',
+};
+
+const options = {
+    fontSize: 14,
+};
+
+export interface EventDetailsProps {
+    // The event.
+    event: EventDto;
+
+    // True to hide all counters.
+    hideCounters?: boolean;
+}
+
+export const EventDetails = React.memo((props: EventDetailsProps) => {
+    const { event } = props;
+
+    return (
+        <AceEditor mode='javascript' style={style} setOptions={options} name='event-details'
+            value={JSON.stringify(event, null, 2)}
+        />
+    );
+});
