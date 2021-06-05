9e5cb8316bb952a3e23dd930f7b8bc0b1081a36d
Sebastian <sebastian@squidex.io>
2021-06-05 20:56:31 +0200

    added missing files.

---
+++ backend/src/Notifo.Domain.Integrations/Mailjet/MailjetEmailServer.cs
@@ -0,0 +1,67 @@
+﻿// ==========================================================================
+//  Notifo.io
+// ==========================================================================
+//  Copyright (c) Sebastian Stehle
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Mailjet.Client;
+using Mailjet.Client.TransactionalEmails;
+using Notifo.Domain.Channels.Email;
+using Notifo.Domain.Integrations.Resources;
+using Notifo.Infrastructure;
+
+namespace Notifo.Domain.Integrations.Mailjet
+{
+    public sealed class MailjetEmailServer : IEmailSender
+    {
+        private readonly MailjetClient mailjetClient;
+
+        public MailjetEmailServer(MailjetClient mailjetClient)
+        {
+            this.mailjetClient = mailjetClient;
+        }
+
+        public async Task SendAsync(EmailMessage message, CancellationToken ct = default)
+        {
+            var email = new TransactionalEmailBuilder()
+                .WithFrom(new SendContact(
+                    message.FromEmail,
+                    message.FromName))
+                .WithTo(new SendContact(
+                    message.ToEmail,
+                    message.ToName))
+                .WithSubject(message.Subject)
+                .WithHtmlPart(message.BodyHtml)
+                .WithTextPart(message.BodyText)
+                .Build();
+
+            var responses = await mailjetClient.SendTransactionalEmailAsync(email);
+
+            if (responses.Messages == null || responses.Messages.Length != 1)
+            {
+                var errorMessage = string.Format(CultureInfo.CurrentCulture, Texts.Mailjet_Error, message.FromEmail);
+
+                throw new DomainException(errorMessage);
+            }
+
+            var response = responses.Messages[0];
+            var responseError = response.Errors?.FirstOrDefault();
+
+            if (!string.IsNullOrWhiteSpace(responseError?.ErrorMessage))
+            {
+                var errorMessage =
+                    string.Format(CultureInfo.CurrentCulture, Texts.Mailjet_Error,
+                        message.FromEmail,
+                        responseError.ErrorMessage,
+                        responseError.ErrorCode);
+
+                throw new DomainException(errorMessage);
+            }
+        }
+    }
+}
---
+++ backend/src/Notifo.Domain.Integrations/Mailjet/MailjetEmailServerPool.cs
@@ -0,0 +1,34 @@
+﻿using Mailjet.Client;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace Notifo.Domain.Integrations.Mailjet
+{
+    internal class MailjetEmailServerPool
+    {
+        private IMemoryCache memoryCache;
+
+        public MailjetEmailServerPool(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public MailjetEmailServer GetServer(string apiKey, string apiSecret)
+        {
+            var cacheKey = $"MailjetEmailServer_{apiKey}_{apiSecret}";
+
+            var found = memoryCache.GetOrCreate(cacheKey, entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
+
+                var sender =
+                    new MailjetEmailServer(
+                        new MailjetClient(apiKey, apiSecret));
+
+                return sender;
+            });
+
+            return found;
+        }
+    }
+}
