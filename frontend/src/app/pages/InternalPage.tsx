7596870fbefcd554ed6712118dc76ffa0785844d
Sebastian <sebastian@squidex.io>
2022-01-17 22:42:53 +0100

    Fix topnav dropdown.

--- frontend/src/app/pages/InternalPage.tsx
+++ frontend/src/app/pages/InternalPage.tsx
@@ -8,12 +8,9 @@
 import * as React from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Switch, useRouteMatch } from 'react-router';
-import { NavLink } from 'react-router-dom';
-import { DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar, UncontrolledDropdown } from 'reactstrap';
 import { ErrorBoundary } from '@app/framework';
-import { AppsDropdown, Logo } from '@app/shared/components';
-import { loadApps, loadLanguages, loadTimezones, logoutStart, useLogin } from '@app/state';
-import { texts } from '@app/texts';
+import { loadApps, loadLanguages, loadTimezones } from '@app/state';
+import { TopNav } from './TopNav';
 import { AppPage } from './app/AppPage';
 import { AppsPage } from './apps/AppsPage';
 import { SystemUsersPage } from './system-users/SystemUsersPage';
@@ -21,7 +18,6 @@ import { SystemUsersPage } from './system-users/SystemUsersPage';
 export const InternalPage = () => {
     const dispatch = useDispatch();
     const match = useRouteMatch();
-    const user = useLogin(x => x.user)!;
 
     React.useEffect(() => {
         dispatch(loadApps());
@@ -29,54 +25,9 @@ export const InternalPage = () => {
         dispatch(loadTimezones());
     }, [dispatch]);
 
-    const doLogout = React.useCallback(() => {
-        dispatch(logoutStart());
-    }, [dispatch]);
-
     return (
         <>
-            <Navbar dark fixed='top' color='primary'>
-                <NavLink to={match.url} className='navbar-brand'>
-                    <Logo />
-                </NavLink>
-
-                <Nav navbar>
-                    <AppsDropdown />
-                </Nav>
-
-                <Nav navbar className='ml-auto'>
-                    <UncontrolledDropdown nav inNavbar>
-                        <DropdownToggle nav caret>
-                            {texts.common.profile}
-                        </DropdownToggle>
-                        <DropdownMenu right>
-                            <DropdownItem>
-                                <div>{texts.common.welcome},</div>
-
-                                <strong>{user.name}</strong>
-                            </DropdownItem>
-
-                            <DropdownItem divider />
-
-                            <DropdownItem href='/account/profile' target='_blank'>
-                                {texts.common.profileSettings}
-                            </DropdownItem>
-
-                            {user.roles.find(x => x.toUpperCase() === 'ADMIN') &&
-                                <NavLink to={`${match.path}/system-users`} className='dropdown-item'>
-                                    {texts.systemUsers.header}
-                                </NavLink>
-                            }
-
-                            <DropdownItem divider />
-
-                            <DropdownItem onClick={doLogout}>
-                                {texts.common.logout}
-                            </DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-                </Nav>
-            </Navbar>
+            <TopNav />
 
             <ErrorBoundary>
                 <Switch>
---
+++ frontend/src/app/pages/TopNav.tsx
@@ -0,0 +1,91 @@
+/*
+ * Notifo.io
+ *
+ * @license
+ * Copyright (c) Sebastian Stehle. All rights reserved.
+ */
+
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import { useRouteMatch } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar } from 'reactstrap';
+import { AppsDropdown, Logo } from '@app/shared/components';
+import { loadApps, loadLanguages, loadTimezones, logoutStart, useLogin } from '@app/state';
+import { texts } from '@app/texts';
+
+export const TopNav = () => {
+    const dispatch = useDispatch();
+    const match = useRouteMatch();
+    const user = useLogin(x => x.user)!;
+    const [isOpen, setOpen] = React.useState(false);
+
+    const doOpen = React.useCallback(() => {
+        setOpen(true);
+    }, []);
+
+    const doClose = React.useCallback(() => {
+        setOpen(false);
+    }, []);
+
+    const doToggle = React.useCallback(() => {
+        setOpen(!isOpen);
+
+        return false;
+    }, [isOpen]);
+
+    React.useEffect(() => {
+        dispatch(loadApps());
+        dispatch(loadLanguages());
+        dispatch(loadTimezones());
+    }, [dispatch]);
+
+    const doLogout = React.useCallback(() => {
+        dispatch(logoutStart());
+    }, [dispatch]);
+
+    return (
+        <Navbar dark fixed='top' color='primary'>
+            <NavLink to={match.url} className='navbar-brand'>
+                <Logo />
+            </NavLink>
+
+            <Nav navbar>
+                <AppsDropdown />
+            </Nav>
+
+            <Nav navbar className='ml-auto'>
+                <Dropdown nav inNavbar isOpen={isOpen} toggle={doToggle}>
+                    <DropdownToggle nav caret onClick={doOpen}>
+                        {texts.common.profile}
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                        <DropdownItem onClick={doClose} href='/account/profile' target='_blank'>
+                            <div>{texts.common.welcome},</div>
+
+                            <strong>{user.name}</strong>
+                        </DropdownItem>
+
+                        <DropdownItem divider />
+
+                        <DropdownItem onClick={doClose} href='/account/profile' target='_blank'>
+                            {texts.common.profileSettings}
+                        </DropdownItem>
+
+                        {user.roles.find(x => x.toUpperCase() === 'ADMIN') &&
+                            <NavLink onClick={doClose} to={`${match.path}/system-users`} className='dropdown-item'>
+                                {texts.systemUsers.header}
+                            </NavLink>
+                        }
+
+                        <DropdownItem divider />
+
+                        <DropdownItem onClick={doLogout}>
+                            {texts.common.logout}
+                        </DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+            </Nav>
+        </Navbar>
+    );
+};
