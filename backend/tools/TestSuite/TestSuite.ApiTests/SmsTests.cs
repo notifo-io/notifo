420864dba7271b8251080ae208a0f04bfe8ebca9
Sebastian <sebastian@squidex.io>
2022-04-22 08:37:13 +0200

    More tests

--- backend/tools/TestSuite/TestSuite.ApiTests/SmsTests.cs
+++ backend/tools/TestSuite/TestSuite.ApiTests/SmsTests.cs
@@ -19,6 +19,9 @@ namespace TestSuite.ApiTests
 {
     public class SmsTests : IClassFixture<ClientFixture>
     {
+        private static readonly string PhoneNumber = "00436703091161";
+        private static readonly string AccessKey = TestHelpers.GetAndPrintValue("messagebird:accessKey", "invalid");
+
         public ClientFixture _ { get; }
 
         public SmsTests(ClientFixture fixture)
@@ -27,13 +30,8 @@ namespace TestSuite.ApiTests
         }
 
         [Fact]
-        public async Task Should_send_SMS()
+        public async Task Should_send_sms_with_template()
         {
-            var phoneNumber = "00436703091161";
-
-            var accessKey = TestHelpers.GetAndPrintValue("messagebird:accessKey", "invalid");
-
-
             var appName = Guid.NewGuid().ToString();
 
             // STEP 0: Create app
@@ -50,7 +48,14 @@ namespace TestSuite.ApiTests
             {
             };
 
-            await _.Client.SmsTemplates.PostTemplateAsync(app_0.Id, smsTemplateRequest);
+            var template_0 = await _.Client.SmsTemplates.PostTemplateAsync(app_0.Id, smsTemplateRequest);
+
+            var smsTemplate = new SmsTemplateDto
+            {
+                Text = "<start>{{ notification.subject }}</end>"
+            };
+
+            await _.Client.SmsTemplates.PutTemplateLanguageAsync(app_0.Id, template_0.Id, "en", smsTemplate);
 
 
             // STEP 2: Create integration
@@ -59,8 +64,8 @@ namespace TestSuite.ApiTests
                 Type = "MessageBird",
                 Properties = new Dictionary<string, string>
                 {
-                    ["accessKey"] = accessKey,
-                    ["phoneNumber"] = phoneNumber,
+                    ["accessKey"] = AccessKey,
+                    ["phoneNumber"] = PhoneNumber,
                     ["phoneNumbers"] = string.Empty
                 },
                 Enabled = true
@@ -76,7 +81,7 @@ namespace TestSuite.ApiTests
                 {
                     new UpsertUserDto
                     {
-                        PhoneNumber = phoneNumber
+                        PhoneNumber = PhoneNumber
                     }
                 }
             };
@@ -117,7 +122,9 @@ namespace TestSuite.ApiTests
 
 
             // Get SMS status
-            var messageBird = Client.CreateDefault(accessKey);
+            var messageBird = Client.CreateDefault(AccessKey);
+
+            var text = $"<start>{subjectId}</end>";
 
             using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(60)))
             {
@@ -125,7 +132,105 @@ namespace TestSuite.ApiTests
                 {
                     var messages = messageBird.ListMessages(string.Empty, 200);
 
-                    if (messages.Items.Any(x => x.Body.Contains(subjectId, StringComparison.OrdinalIgnoreCase) && x.Recipients.Items[0].Status == Recipient.RecipientStatus.Delivered))
+                    if (messages.Items.Any(x => x.Body == text && x.Recipients.Items[0].Status == Recipient.RecipientStatus.Delivered))
+                    {
+                        return;
+                    }
+
+                    await Task.Delay(1000);
+                }
+            }
+
+            Assert.False(true, "SMS not sent.");
+        }
+
+        [Fact]
+        public async Task Should_send_sms_without_template()
+        {
+            var appName = Guid.NewGuid().ToString();
+
+            // STEP 0: Create app
+            var createRequest = new UpsertAppDto
+            {
+                Name = appName
+            };
+
+            var app_0 = await _.Client.Apps.PostAppAsync(createRequest);
+
+
+            // STEP 1: Create integration
+            var emailIntegrationRequest = new CreateIntegrationDto
+            {
+                Type = "MessageBird",
+                Properties = new Dictionary<string, string>
+                {
+                    ["accessKey"] = AccessKey,
+                    ["phoneNumber"] = PhoneNumber,
+                    ["phoneNumbers"] = string.Empty
+                },
+                Enabled = true
+            };
+
+            await _.Client.Apps.PostIntegrationAsync(app_0.Id, emailIntegrationRequest);
+
+
+            // STEP 2: Create user
+            var userRequest = new UpsertUsersDto
+            {
+                Requests = new List<UpsertUserDto>
+                {
+                    new UpsertUserDto
+                    {
+                        PhoneNumber = PhoneNumber
+                    }
+                }
+            };
+
+            var users_0 = await _.Client.Users.PostUsersAsync(app_0.Id, userRequest);
+            var user_0 = users_0.First();
+
+
+            // STEP 3: Send SMS
+            var subjectId = Guid.NewGuid().ToString();
+
+            var publishRequest = new PublishManyDto
+            {
+                Requests = new List<PublishDto>
+                {
+                    new PublishDto
+                    {
+                        Topic = $"users/{user_0.Id}",
+                        Preformatted = new NotificationFormattingDto
+                        {
+                            Subject = new LocalizedText
+                            {
+                                ["en"] = subjectId
+                            }
+                        },
+                        Settings = new Dictionary<string, ChannelSettingDto>
+                        {
+                            [Providers.Sms] = new ChannelSettingDto
+                            {
+                                Send = ChannelSend.Send
+                            }
+                        }
+                    }
+                }
+            };
+
+            await _.Client.Events.PostEventsAsync(app_0.Id, publishRequest);
+
+
+            // Get SMS status
+            var messageBird = Client.CreateDefault(AccessKey);
+
+            using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(60)))
+            {
+                while (!cts.IsCancellationRequested)
+                {
+                    var messages = messageBird.ListMessages(string.Empty, 200);
+
+                    if (messages.Items.Any(x => x.Body == subjectId && x.Recipients.Items[0].Status == Recipient.RecipientStatus.Delivered))
                     {
                         return;
                     }
