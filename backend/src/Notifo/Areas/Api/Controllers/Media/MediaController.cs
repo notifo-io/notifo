4c7abaa59223bdd5d4e9ba5a1e4ce958701ab55b
Sebastian <sebastian@squidex.io>
2022-09-07 16:30:44 +0200

    Proxy controller for images.

--- backend/src/Notifo/Areas/Api/Controllers/Media/MediaController.cs
+++ backend/src/Notifo/Areas/Api/Controllers/Media/MediaController.cs
@@ -7,25 +7,20 @@
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Net.Http.Headers;
 using Notifo.Areas.Api.Controllers.Media.Dtos;
 using Notifo.Domain.Identity;
 using Notifo.Domain.Media;
-using Notifo.Infrastructure;
 using Notifo.Infrastructure.Validation;
 using Notifo.Pipeline;
 using NSwag.Annotations;
 using Squidex.Assets;
 using Squidex.Hosting;
-using MediaItem = Notifo.Domain.Media.Media;
 
 namespace Notifo.Areas.Api.Controllers.Media
 {
     [OpenApiTag("Media")]
-    public sealed class MediaController : BaseController
+    public sealed class MediaController : MediaBaseController
     {
-        private readonly IAssetStore assetStore;
-        private readonly IAssetThumbnailGenerator assetThumbnailGenerator;
         private readonly IMediaStore mediaStore;
         private readonly IMediaFileStore mediaFileStore;
         private readonly IUrlGenerator urlGenerator;
@@ -36,9 +31,8 @@ namespace Notifo.Areas.Api.Controllers.Media
             IMediaStore mediaStore,
             IMediaFileStore mediaFileStore,
             IUrlGenerator urlGenerator)
+            : base(assetStore, assetThumbnailGenerator)
         {
-            this.assetStore = assetStore;
-            this.assetThumbnailGenerator = assetThumbnailGenerator;
             this.mediaStore = mediaStore;
             this.mediaFileStore = mediaFileStore;
             this.urlGenerator = urlGenerator;
@@ -79,13 +73,31 @@ namespace Notifo.Areas.Api.Controllers.Media
         /// 404 => Media or app not found.
         /// </returns>
         [HttpGet("api/apps/{appId:notEmpty}/media/{fileName:notEmpty}")]
+        [HttpGet("api/asset/{appId:notEmpty}/{fileName:notEmpty}")]
         [HttpGet("api/assets/{appId:notEmpty}/{fileName:notEmpty}")]
         [AllowAnonymous]
         public async Task<IActionResult> Download(string appId, string fileName, [FromQuery] MediaFileQueryDto? query = null)
         {
             var media = await mediaStore.GetAsync(appId, fileName, HttpContext.RequestAborted);
 
-            return DeliverAsset(appId, media, query);
+            if (media == null)
+            {
+                return NotFound();
+            }
+
+            var source = new ResizeSource
+            {
+                FileId = $"{appId}_{media.FileName}",
+                FileName = media.FileName,
+                FileSize = media.FileSize,
+                MimeType = media.MimeType,
+                OpenRead = (stream, ct) =>
+                {
+                    return mediaFileStore.DownloadAsync(appId, media, stream, default, ct);
+                }
+            };
+
+            return DeliverAsset(source, query);
         }
 
         /// <summary>
@@ -124,136 +136,6 @@ namespace Notifo.Areas.Api.Controllers.Media
             await mediaStore.DeleteAsync(appId, fileName, HttpContext.RequestAborted);
 
             return NoContent();
-        }
-
-        private IActionResult DeliverAsset(string appId, MediaItem? media, MediaFileQueryDto? query)
-        {
-            query ??= new MediaFileQueryDto();
-
-            if (media == null)
-            {
-                return NotFound();
-            }
-
-            var resizeOptions = query.ToResizeOptions();
-
-            if (query.CacheDuration > 0)
-            {
-                Response.Headers[HeaderNames.CacheControl] = $"public,max-age={query.CacheDuration}";
-            }
-
-            var contentLength = (long?)null;
-            var contentCallback = (FileCallback?)null;
-            var contentType = media.MimeType;
-
-            if (media.Type == MediaType.Image && assetThumbnailGenerator.IsResizable(media.MimeType, resizeOptions, out var destinationMimeType))
-            {
-                contentType = destinationMimeType;
-                contentCallback = async (bodyStream, range, ct) =>
-                {
-                    var resizedAsset = $"{appId:notEmpty}_{media.FileName}_{resizeOptions:notEmpty}";
-
-                    if (query.ForceResize)
-                    {
-                        await ResizeAsync(appId, media, bodyStream, resizedAsset, resizeOptions, true, ct);
-                    }
-                    else
-                    {
-                        try
-                        {
-                            await assetStore.DownloadAsync(resizedAsset, bodyStream, ct: ct);
-                        }
-                        catch (AssetNotFoundException)
-                        {
-                            await ResizeAsync(appId, media, bodyStream, resizedAsset, resizeOptions, false, ct);
-                        }
-                    }
-                };
-            }
-            else
-            {
-                contentLength = media.FileSize;
-
-                contentCallback = async (bodyStream, range, ct) =>
-                {
-                    await mediaFileStore.DownloadAsync(appId, media, bodyStream, range, ct);
-                };
-            }
-
-            return new FileCallbackResult(media.MimeType, contentCallback)
-            {
-                EnableRangeProcessing = contentLength > 0,
-                ErrorAs404 = true,
-                FileDownloadName = media.FileName,
-                FileSize = contentLength,
-                SendInline = query.Download != 1
-            };
-        }
-
-        private async Task ResizeAsync(string appId, MediaItem media, Stream target, string fileName, ResizeOptions resizeOptions, bool overwrite,
-            CancellationToken ct)
-        {
-#pragma warning disable MA0040 // Flow the cancellation token
-            using var activity = Telemetry.Activities.StartActivity("Resize");
-
-            await using var assetOriginal = new TempAssetFile(media.FileName, media.MimeType, 0);
-            await using var assetResized = new TempAssetFile(media.FileName, media.MimeType, 0);
-
-            using (Telemetry.Activities.StartActivity("Read"))
-            {
-                await using (var originalStream = assetOriginal.OpenWrite())
-                {
-                    await mediaFileStore.DownloadAsync(appId, media, originalStream, default, ct);
-                }
-            }
-
-            using (Telemetry.Activities.StartActivity("Resize"))
-            {
-                try
-                {
-                    await using (var originalStream = assetOriginal.OpenRead())
-                    {
-                        await using (var resizeStream = assetResized.OpenWrite())
-                        {
-                            await assetThumbnailGenerator.CreateThumbnailAsync(originalStream, media.MimeType, resizeStream, resizeOptions);
-                        }
-                    }
-                }
-                catch
-                {
-                    await using (var originalStream = assetOriginal.OpenRead())
-                    {
-                        await using (var resizeStream = assetResized.OpenWrite())
-                        {
-                            await originalStream.CopyToAsync(resizeStream);
-                        }
-                    }
-                }
-            }
-
-            using (Telemetry.Activities.StartActivity("Save"))
-            {
-                try
-                {
-                    await using (var resizeStream = assetResized.OpenRead())
-                    {
-                        await assetStore.UploadAsync(fileName, resizeStream, overwrite);
-                    }
-                }
-                catch (AssetAlreadyExistsException)
-                {
-                    return;
-                }
-            }
-
-            using (Telemetry.Activities.StartActivity("Write"))
-            {
-                await using (var resizeStream = assetResized.OpenRead())
-                {
-                    await resizeStream.CopyToAsync(target, ct);
-                }
-            }
-#pragma warning restore MA0040 // Flow the cancellation token
         }
 
         private static AssetFile CreateFile(IFormFile file)
