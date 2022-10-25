54d39f3407eab609f0e1a206cfcb0415bc55e620
Sebastian Stehle <sebastian@squidex.io>
2022-10-25 16:42:48 +0200

    Update versions. (#136)
    * Update versions.
    
    * Update tests.
    
    * Add OTLP sampling.

--- backend/src/Notifo.Infrastructure/Json/JsonServiceExtensions.cs
+++ backend/src/Notifo.Infrastructure/Json/JsonServiceExtensions.cs
@@ -26,7 +26,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services.AddSingleton(options);
 
-            services.AddSingletonAs(c => new SystemTextJsonTransportSerializer(c => Configure(c, configure)))
+            services.AddSingletonAs(c => new SystemTextJsonMessagingSerializer(c => Configure(c, configure)))
                 .As<IMessagingSerializer>();
 
             services.AddSingletonAs<SystemTextJsonSerializer>()
