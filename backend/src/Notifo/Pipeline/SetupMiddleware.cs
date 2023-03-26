5aef6f580a0b2017d29ede4340282559e5d6433a
Sebastian <sebastian@squidex.io>
2023-03-26 19:03:54 +0200

    Better html transform.

--- backend/src/Notifo/Pipeline/SetupMiddleware.cs
+++ backend/src/Notifo/Pipeline/SetupMiddleware.cs
@@ -21,6 +21,12 @@ public sealed class SetupMiddleware
 
     public async Task InvokeAsync(HttpContext context, IUserService userService)
     {
+        if (!IsSpaPath(context) || IsDevServer(context))
+        {
+            await next(context);
+            return;
+        }
+
         if (!isUserFound && await userService.IsEmptyAsync(context.RequestAborted))
         {
             var url = context.Request.PathBase.Add("/account/setup");
@@ -33,5 +39,15 @@ public sealed class SetupMiddleware
 
             await next(context);
         }
+    }
+
+    private static bool IsSpaPath(HttpContext context)
+    {
+        return context.IsIndex() || !Path.HasExtension(context.Request.Path);
+    }
+
+    private static bool IsDevServer(HttpContext context)
+    {
+        return context.Request.Path.StartsWithSegments("/ws", StringComparison.OrdinalIgnoreCase);
     }
 }
