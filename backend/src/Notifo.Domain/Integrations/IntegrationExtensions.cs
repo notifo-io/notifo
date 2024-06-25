573c09703c70a68c39f58ea20b07d54d47fbaf35
Artur Nowak <54641159+Arciiix@users.noreply.github.com>
2024-06-25 13:35:00 +0200

    Fix adding user properties to UserInfo context (#243)
    * Fix updating the user preferences
    
    * Fix adding user properties to UserInfo context
    
    * Fix adding user properties to UserInfo context
    
    * Make the dictionaries merge code more imperative

--- backend/src/Notifo.Domain/Integrations/IntegrationExtensions.cs
+++ backend/src/Notifo.Domain/Integrations/IntegrationExtensions.cs
@@ -6,6 +6,8 @@
 // ==========================================================================
 
 using Notifo.Domain.Users;
+using Notifo.Infrastructure.Collections;
+using System.Collections.Generic;
 
 namespace Notifo.Domain.Integrations;
 
@@ -13,6 +15,22 @@ public static class IntegrationExtensions
 {
     public static UserInfo ToContext(this User user)
     {
-        return new UserInfo { Id = user.Id, EmailAddress = user.EmailAddress, PhoneNumber = user.PhoneNumber, Properties = user.SystemProperties };
+        var properties = new Dictionary<string, string>(user.Properties);
+
+        if (user.SystemProperties != null)
+        {
+            foreach (var (key, value) in user.SystemProperties)
+            {
+                properties[key] = value;
+            }
+        }
+
+        return new UserInfo
+        {
+            Id = user.Id,
+            EmailAddress = user.EmailAddress,
+            PhoneNumber = user.PhoneNumber,
+            Properties = properties.ToReadonlyDictionary()
+        };
     }
 }
